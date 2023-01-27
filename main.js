c825a6dd10e5cfbe8afcba5d7c52ac2fdaed0ea9
ermckane <ermckane@gmail.com>
2023-01-26 20:44:29 -0500

    Added animated hamburger button to header in place of old one. Hamburger menu and overlay linked as well

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const headerNavButton = document.querySelector('.header-nav-button');
+const hamburgerNav = document.querySelector('#hamburger-nav')
+const overlay = document.querySelector('.overlay')
 
 console.log(headerNavButton)
 
@@ -7,7 +9,11 @@ headerNavButton.addEventListener("click", () => {
 
     if (isOpened === "false") {
     headerNavButton.setAttribute("aria-expanded", "true");
+    hamburgerNav.setAttribute('aria-expanded', 'true');
+    overlay.setAttribute('aria-expanded', 'true');
     } else {
     headerNavButton.setAttribute("aria-expanded", "false");
+    hamburgerNav.setAttribute("aria-expanded", "false");
+    overlay.setAttribute("aria-expanded", "false");
     }
 });
